c87c38c8b34e8c9a7a6ddd139ea501b577a0a34d
millette17 <mwmillette@gmail.com>
2016-12-30 10:51:10 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera Programming Assignment #2
 
-## Write a short comment describing this function
+## Assignment: Caching the Inverse of a Matrix
+
+## Matrix inversion is usually a costly computation and there may be some benefit
+## to caching the inverse of a matrix rather than compute it repeatedly
+## (there are also alternatives to matrix inversion that we will not discuss here).
+## Your assignment is to write a pair of functions that cache the inverse of a matrix.
+
+## Function 1 - makeCacheMatrix: This function creates a special 
+## "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+        imat <- NULL
+        set <- function(y) {
+          x <<- y
+          mat <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) imat <<- inverse
+        getInverse <- function() imat
+        list(set=set,
+             get=get,
+             setInverse=setInverse,
+             getInverse=getInverse)
+        }
 
-}
 
-
-## Write a short comment describing this function
+## Function 2 - cacheSolve: This function computes the inverse of the special "matrix" returned
+## by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not 
+## changed), then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        imat <- x$getInverse()
+        if (!is.null(imat)) {
+              message("retrieving data")
+              return(imat)
+        }
+        xmat <- x$get()
+        imat <- solve(xmat, ...)
+        x$setInverse(imat)
+        imat
+  }
